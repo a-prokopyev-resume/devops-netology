985407973e1ecd80cae1c3a8a7e29a7827bab29c
Alexander Prokopyev <a.prokopyev.resume@gmail.com>
2023-11-23 09:03:31 +0500

    Test branching for PR

--- 07-terraform-new/04/src/main.tf
+++ 07-terraform-new/04/src/main.tf
@@ -10,6 +10,21 @@ resource "yandex_vpc_subnet" "develop" {
 }
 */
 
+ backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "firstbacket1"
+    region   = "ru-central1"
+    key      = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+
+        dynamodb_endpoint = ""
+        dynamodb_table    = "terraform-lock"
+  }
+}
+provider "yan
+
 module "udjin10-module" {
   source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name        = "develop"
